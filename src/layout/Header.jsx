05e1d439ca321ab00a1318cfbf23cf6e878a96c6
Nimrod Garcia <nimm.garcia@gmail.com>
2019-04-06 21:18:12 -0600

    Search in movies/comics, random 10 cards on load, show all btn

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -6,15 +6,40 @@ export default class Header extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      value: ''
+      value: '',
+      searchCriteria: 'title',
+      searchThisDataset: '',
+      setSearchCriteria: this.setSearchCriteria,
+      setSearchDataset: this.setSearchDataset
     }
+  }
+
+  // future use to set search criteria
+  setSearchCriteria = criteria => {
+    this.setState({searchCriteria: criteria})
+  }
+
+  setSearchDataset = (dataset) => {
+    this.setState({searchThisDataset: dataset})
   }
 
   render() {
     return (
       <header className='Header'>
-        <Search data ={this.props.combined} storeRendered={this.props.storeRendered}/>
-        <Filter storeRendered={this.props.storeRendered} comics={this.props.comics} movies={this.props.movies}/>
+        <Search 
+          data={this.props.combined} 
+          movies={this.props.movies}
+          comics={this.props.comics}
+          storeRendered={this.props.storeRendered}
+          {...this.state}
+        />
+        <Filter 
+          data={this.props.combined}
+          storeRendered={this.props.storeRendered} 
+          comics={this.props.comics} 
+          movies={this.props.movies}
+          {...this.state}
+        />
       </header>
     )
   }
