60749a313f8c2829c7935a97891f0af7ce3a63db
Max Silver <Maxbsilver@gmail.com>
2019-04-07 12:23:38 -0600

    Grid card-container and card styling

--- src/components/card components/card/Card.jsx
+++ src/components/card components/card/Card.jsx
@@ -14,12 +14,13 @@ export class Card extends Component {
   render() {
     return (
       (<article className='card'>
+       <div className ='card-positioning'>
         <img src={this.props.card.img} 
           alt={`${this.props.card.title}`} 
-          width="200px"
           onClick={this.showCardInfo}/>
         { this.state.toggleInfo && <div><a target="blank" href={this.props.card.link} >{this.props.card.link}</a></div> }
           <h3>{this.props.card.title}</h3>
+          </div>
       </article>)
     )
   }
