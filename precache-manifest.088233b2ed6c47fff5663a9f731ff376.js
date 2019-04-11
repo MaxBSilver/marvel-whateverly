04ceb718fea3698bb56a1352637a5760c9b27f9a
Nimrod Garcia <nimm.garcia@gmail.com>
2019-04-11 08:54:22 -0600

    Updates

---
+++ precache-manifest.088233b2ed6c47fff5663a9f731ff376.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f77e7d8ab7e73e999c3",
+    "url": "/MaxBSilver/marvel-whateverly/static/css/main.60d9268e.chunk.css"
+  },
+  {
+    "revision": "5f77e7d8ab7e73e999c3",
+    "url": "/MaxBSilver/marvel-whateverly/static/js/main.8a9e014b.chunk.js"
+  },
+  {
+    "revision": "2e7010f5f7ac962ab73b",
+    "url": "/MaxBSilver/marvel-whateverly/static/js/runtime~main.b102f200.js"
+  },
+  {
+    "revision": "123fb41c3fb46a6d1e24",
+    "url": "/MaxBSilver/marvel-whateverly/static/js/2.a5adb38c.chunk.js"
+  },
+  {
+    "revision": "506651dd2fe02950f8265bed29c2d0be",
+    "url": "/MaxBSilver/marvel-whateverly/index.html"
+  }
+];
